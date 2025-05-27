a2c1eb35c4220154a5a0ec79ee68a1d2b4369601
Fromont-L <lucasfromont97@gmail.com>
2025-05-27 15:50:13 +0200

    Méthode POST dans le controleur

--- src/main/java/fr/diginamic/hello/controleur/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controleur/VilleControleur.java
@@ -1,9 +1,9 @@
 package fr.diginamic.hello.controleur;
 
 import fr.diginamic.hello.Ville;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,14 +13,30 @@ import java.util.List;
 @RequestMapping("villes")
 public class VilleControleur {
 
+    private List<Ville> villes = new ArrayList<>();
+
+    public VilleControleur() {
+        villes.add(new Ville("Thézan-des-Corbières", 560));
+        villes.add(new Ville("Villerouge la Crémade", 140));
+        villes.add(new Ville("Montquc", 1241));
+        villes.add(new Ville("Anus", 130));
+        villes.add(new Ville("Mouais", 356));
+    }
+
     @GetMapping
     public List<Ville> getVilles() {
-        return Arrays.asList(
-                new Ville("Thézan-des-Corbières", 560),
-                new Ville("Villerouge la Crémade", 140),
-                new Ville("Montquc", 1241),
-                new Ville("Anus", 130),
-                new Ville("Mouais", 356)
-        );
+        return villes;
     }
+
+    @PostMapping
+    public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille) {
+        boolean existe = villes.stream().anyMatch(v -> v.getNom().equalsIgnoreCase(nouvelleVille.getNom()));
+        if (existe) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("La ville est déjà existante");
+        }
+
+        villes.add(nouvelleVille);
+        return ResponseEntity.ok("Ville insérée avec succès");
+    }
+
 }
